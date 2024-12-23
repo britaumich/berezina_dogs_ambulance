c800ab9a28006b5b021beda1aaa840b0bdaee37f
Margarita Barvinok <brita@umich.edu>
2024-12-23 10:04:15 -0500

    add authentication

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  resource :registration, only: [:new,:create]
+  resource :session
+  resources :passwords, param: :token
+  get 'pages/about'
+  get 'pages/authentification'
+  get 'pages/account'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
@@ -7,5 +13,4 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   root "home#index"
-  get "/"
 end
