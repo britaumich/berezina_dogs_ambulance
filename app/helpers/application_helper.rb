9ecb9ff3249fe36ccbabb3ecb7e23618c9a3795d
Margarita Barvinok <brita@umich.edu>
2024-12-28 13:35:19 -0500

    calculate age

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -15,5 +15,29 @@ module ApplicationHelper
       animal.id
     end
   end
+
+  def age(animal)
+    if animal.birth_year.present?
+      dob = animal.birth_year
+      now = Date.today
+      months = (now.year * 12 + now.month) - (dob.year * 12 + dob.month)
+
+      # months / 12 will give the number of years
+      # months % 12 will give the number of months
+      readable_age(months / 12, months % 12)
+    else
+      "N/A"
+    end
+  end
+
+  def readable_age(years, months)
+    year_text = ''
+    if years != 0
+      year_text = "#{pluralize(years, 'year')} "
+    end
+  
+    "#{year_text}#{pluralize(months, 'month')}"
+  end
+  
   
 end
