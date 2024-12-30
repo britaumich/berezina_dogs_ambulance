28322a004f4b9a297d938f47b7198395eda07a2d
Margarita Barvinok <brita@umich.edu>
2024-12-29 23:06:21 -0500

    add description to medical procedures

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2024_12_30_031041) do
+ActiveRecord::Schema[8.0].define(version: 2024_12_30_033308) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -97,6 +97,7 @@ ActiveRecord::Schema[8.0].define(version: 2024_12_30_031041) do
     t.bigint "procedure_type_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "description"
     t.index ["animal_id"], name: "index_medical_procedures_on_animal_id"
     t.index ["procedure_type_id"], name: "index_medical_procedures_on_procedure_type_id"
   end
