e68f5888b89ebca97e6d0b2bef600d0cbe38cffc
Margarita Barvinok <brita@umich.edu>
2025-04-19 20:25:57 -0400

    remove prints

--- app/javascript/controllers/flatpickr_controller.js
+++ app/javascript/controllers/flatpickr_controller.js
@@ -4,7 +4,6 @@ import flatpickr from "flatpickr"
 // Explicitly define the controller class and export it
 export default class extends Controller {
   connect() {
-    console.log("date contr")
     this.fp = flatpickr(this.element, {
       dateFormat: "d-m-Y",
       allowInput: false,
@@ -12,7 +11,6 @@ export default class extends Controller {
       required: true,
       disableMobile: true,
       onChange: (selectedDates, dateStr) => {
-        console.log(dateStr)
         if (!dateStr) {
           this.element.setCustomValidity("Please select a date")
         } else {
