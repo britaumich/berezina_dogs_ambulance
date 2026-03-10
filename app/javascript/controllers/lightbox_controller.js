37190f05cd790f9c1234d727a2d8f63fe76c498d
Margarita Barvinok <brita@umich.edu>
2026-03-10 15:46:23 -0400

    do not need that

--- app/javascript/controllers/lightbox_controller.js
+++ app/javascript/controllers/lightbox_controller.js
@@ -2,7 +2,6 @@ import { Controller } from "@hotwired/stimulus"
 import GLightbox from "glightbox"
 
 export default class extends Controller {
-  static values = { index: Number }
   
   connect() {
     console.log("Lightbox controller connected")
