04d5f4e648228002f65d91bfd650a40af549aa08
Margarita Barvinok <brita@umich.edu>
2026-03-12 21:16:19 -0400

    check to have one admin

--- app/models/admin_user.rb
+++ app/models/admin_user.rb
@@ -10,6 +10,7 @@
 #
 class AdminUser < ApplicationRecord
   before_destroy :one_admin_user_should_exist
+  before_update :prevent_demoting_last_admin
   enum :role, [:admin, :employee], prefix: true, scopes: true
 
   normalizes :email, with: ->(e) { e.strip.downcase }
@@ -25,4 +26,18 @@ class AdminUser < ApplicationRecord
       throw(:abort)
     end
   end
+
+  def prevent_demoting_last_admin
+     # Only relevant if this record is currently the last admin and is being demoted.
+     role_changing_from_admin_to_non_admin =
+       if respond_to?(:will_save_change_to_role?)
+         will_save_change_to_role? && role_was == "admin" && !role_admin?
+       else
+         respond_to?(:role_changed?) && role_changed? && role_was == "admin" && !role_admin?
+       end
+     if role_changing_from_admin_to_non_admin && AdminUser.role_admin.count == 1
+       errors.add(:base, I18n.t("activerecord.errors.models.admin_user.one_admin_user_must_exist"))
+       throw(:abort)
+     end
+   end
 end 
